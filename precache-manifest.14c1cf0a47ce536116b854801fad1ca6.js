9ac5fc6c4db1420e15ec471c620cfd15cfe7cbdc
katia1802 <katia.rojas.sandoval@gmail.com>
2019-03-18 13:02:22 +0100

    Updates

---
+++ precache-manifest.14c1cf0a47ce536116b854801fad1ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d1a43b0a6ea49b8950c6599f8f70ed7",
+    "url": "/brastlewark-population/static/media/cover.3d1a43b0.jpg"
+  },
+  {
+    "revision": "6314d9f128931eb42ca7",
+    "url": "/brastlewark-population/static/js/runtime~main.78f0b4ee.js"
+  },
+  {
+    "revision": "82bdaff7da1d361c7a89",
+    "url": "/brastlewark-population/static/js/main.425127d7.chunk.js"
+  },
+  {
+    "revision": "1fcdc4008688a1f07024",
+    "url": "/brastlewark-population/static/js/2.05d16d92.chunk.js"
+  },
+  {
+    "revision": "82bdaff7da1d361c7a89",
+    "url": "/brastlewark-population/static/css/main.dc717191.chunk.css"
+  },
+  {
+    "revision": "a8174aad3d2f5006cb2c1feebadc3e26",
+    "url": "/brastlewark-population/index.html"
+  }
+];
