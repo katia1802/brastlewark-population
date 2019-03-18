743e320b95f73a992e07648263b00abb50f8ec01
katia1802 <katia.rojas.sandoval@gmail.com>
2019-03-18 12:04:19 +0100

    changing build for docs

---
+++ docs/build/precache-manifest.e013a1539a2720aa6e59446ca1ab7da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d1a43b0a6ea49b8950c6599f8f70ed7",
+    "url": "/static/media/cover.3d1a43b0.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9922ee04c34b8320e712",
+    "url": "/static/js/main.98df15f3.chunk.js"
+  },
+  {
+    "revision": "1fcdc4008688a1f07024",
+    "url": "/static/js/2.05d16d92.chunk.js"
+  },
+  {
+    "revision": "9922ee04c34b8320e712",
+    "url": "/static/css/main.d1f58453.chunk.css"
+  },
+  {
+    "revision": "f0b02f563ab24f9c3381f7a14d99f943",
+    "url": "/index.html"
+  }
+];
